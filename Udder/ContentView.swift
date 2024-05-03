9c42d565ee519eb3185fdda9343361505533c167
Soki Nakamura <1711023h@gmail.com>
2024-05-03 17:57:57 +0900

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by 中村創己 on 2024/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Udder")
+            Text("Freshmilk")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by 中村創己 on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
